327fd6f21c7eb6274fad4eee53666f274a2b44e4
Bruce Lee <coolcode@live.com>
2019-07-04 20:35:53 +1000

    :monkey_face: setup 2 clusters

---
+++ Nnodes/setup_1.sh
@@ -0,0 +1,198 @@
+#!/bin/bash
+
+#
+# Create all the necessary scripts, keys, configurations etc. to run
+# a cluster of N Quorum nodes with Raft consensus.
+#
+# The nodes will be in Docker containers. List the IP addresses that
+# they will run at below (arbitrary addresses are fine).
+#
+# Run the cluster with "docker-compose up -d"
+#
+# Run a console on Node N with "geth attach qdata_N/dd/geth.ipc"
+# (assumes Geth is installed on the host.)
+#
+# Geth and Constellation logfiles for Node N will be in qdata_N/logs/
+#
+
+# TODO: check file access permissions, especially for keys.
+
+
+#### Configuration options #############################################
+
+# One Docker container will be configured for each IP address in $ips
+subnet="172.13.0.0/24"
+number_of_node=$1
+offest=$2
+begin_index=$(( $2 + 1 )) 
+echo 'parameters: '$number_of_node' '$offest' '$begin_index'. '
+#ips=("172.13.0.15" "172.13.0.2" "172.13.0.3")
+ips=()
+x=$begin_index
+while [ $x -le $number_of_node ]
+do
+  x=$(( $x + 1 )) 
+  # begins with 172.13.0.2
+  ips+=("172.13.0.$x")
+done
+
+# Docker image name
+image=yopoo/quorum
+
+########################################################################
+
+nnodes=${#ips[@]}
+
+#echo '[1] '$nnodes' nodes.'
+
+if [[ $nnodes < 02 ]]
+then
+    echo "ERROR: There must be more than one node IP address."
+    exit 1
+fi
+   
+./cleanup.sh
+
+uid=`id -u`
+gid=`id -g`
+pwd=`pwd`
+
+#### Create directories for each node's configuration ##################
+
+echo '[1] Configuring for '$nnodes' nodes.'
+
+#exit 1
+
+n=$begin_index 
+for ip in ${ips[*]}
+do
+    qd=qdata_$n
+    mkdir -p $qd/{logs,keys}
+    mkdir -p $qd/dd/geth
+
+    let n++
+done
+
+
+#### Make static-nodes.json and store keys #############################
+
+echo '[2] Creating Enodes and static-nodes.json.'
+
+echo "[" > static-nodes.json
+n=$begin_index
+for ip in ${ips[*]}
+do
+    qd=qdata_$n
+
+    # Generate the node's Enode and key
+    enode=`docker run -u $uid:$gid -v $pwd/$qd:/qdata $image /usr/local/bin/bootnode -genkey /qdata/dd/nodekey`
+    enode=`docker run -u $uid:$gid -v $pwd/$qd:/qdata $image /usr/local/bin/bootnode -nodekey /qdata/dd/nodekey -writeaddress`
+
+    # Add the enode to static-nodes.json
+    echo ' '$enode'@'$ip' '
+    sep=`[[ $n !=  $nnodes ]] && echo ","`
+    echo '  "enode://'$enode'@'$ip':30303?discport=0&raftport=50400"'$sep >> static-nodes.json
+
+    let n++
+done
+echo "]" >> static-nodes.json
+
+
+#### Create accounts, keys and genesis.json file #######################
+
+echo '[3] Creating Ether accounts and genesis.json.'
+
+cat > genesis.json <<EOF
+{
+  "alloc": {
+EOF
+
+n=$begin_index
+for ip in ${ips[*]}
+do
+    qd=qdata_$n
+
+    # Generate an Ether account for the node
+    touch $qd/passwords.txt
+    account=`docker run -u $uid:$gid -v $pwd/$qd:/qdata $image /usr/local/bin/geth --datadir=/qdata/dd --password /qdata/passwords.txt account new | cut -c 11-50`
+
+    # Add the account to the genesis block so it has some Ether at start-up
+    sep=`[[ $n != $nnodes ]] && echo ","`
+    cat >> genesis.json <<EOF
+    "0x${account}": {
+      "balance": "1000000000000000000000000000"
+    }${sep}
+EOF
+
+    let n++
+done
+
+cat >> genesis.json <<EOF
+  },
+  "coinbase": "0x0000000000000000000000000000000000000000",
+  "config": {
+    "byzantiumBlock": 1,
+    "chainId": 10,
+    "eip150Block": 1,
+    "eip155Block": 0,
+    "eip150Hash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+    "eip158Block": 1,
+    "isQuorum":true
+  },
+  "difficulty": "0x0",
+  "extraData": "0x",
+  "gasLimit": "0x2FEFD800",
+  "mixhash": "0x00000000000000000000000000000000000000647572616c65787365646c6578",
+  "nonce": "0x0",
+  "parentHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+  "timestamp": "0x00"
+}
+EOF
+
+
+#### Make node list for tm.conf ########################################
+
+nodelist=
+n=$begin_index
+for ip in ${ips[*]}
+do
+    sep=`[[ $ip != ${ips[0]} ]] && echo ","`
+    nodelist=${nodelist}${sep}'"http://'${ip}':9000/"'
+    let n++
+done
+
+
+#### Complete each node's configuration ################################
+
+echo '[4] Creating Quorum keys and finishing configuration.'
+
+n=$begin_index
+for ip in ${ips[*]}
+do
+    qd=qdata_$n
+
+    cat templates/tm.conf \
+        | sed s/_NODEIP_/${ips[$((n-1))]}/g \
+        | sed s%_NODELIST_%$nodelist%g \
+              > $qd/tm.conf
+
+    cp genesis.json $qd/genesis.json
+    cp static-nodes.json $qd/dd/static-nodes.json
+
+    # Generate Quorum-related keys (used by Constellation)
+    docker run -u $uid:$gid -v $pwd/$qd:/qdata $image /usr/local/bin/constellation-node  --generatekeys=/qdata/keys/tm < /dev/null > /dev/null
+    docker run -u $uid:$gid -v $pwd/$qd:/qdata $image /usr/local/bin/constellation-node  --generatekeys=/qdata/keys/tma < /dev/null > /dev/null
+    # echo 'Node '$n' public key: '`cat $qd/keys/tm.pub`
+
+    if [ $n -eq 1 ]; then
+      cp static-nodes.json $qd/dd/permissioned-nodes.json
+      cp templates/start-node-permission.sh $qd/start-node.sh
+      echo 'Node '$n': permissioned' 
+    else
+      cp templates/start-node.sh $qd/start-node.sh
+    fi
+    chmod 755 $qd/start-node.sh
+
+    let n++
+done
+
---
+++ Nnodes/setup_2_clusters.sh
@@ -0,0 +1,67 @@
+./setup_1.sh 7 0
+./setup_2.sh 3 7
+
+number_of_node=10
+echo 'All public keys:'
+n=1
+while [ $n -le $number_of_node ]
+do
+  n=$(( $n + 1 )) 
+  # begins with 172.13.0.2
+  qd=qdata_$n
+  echo '"'`cat $qd/keys/tm.pub`'",'
+done
+
+rm -rf genesis.json static-nodes.json
+
+
+#### Create the docker-compose file ####################################
+
+cat > docker-compose.yml <<EOF
+version: '2'
+services:
+EOF
+
+n=1
+while [ $n -le $number_of_node ]
+do
+    qd=qdata_$n
+
+    cat >> docker-compose.yml <<EOF
+  node_$n:
+    image: $image
+    volumes:
+      - './$qd:/qdata'
+    networks:
+      quorum_net:
+        ipv4_address: '$ip'
+    ports:
+      - $((n+22000)):8545
+    user: '$uid:$gid'
+EOF
+
+    let n++
+done
+
+cat >> docker-compose.yml <<EOF
+
+networks:
+  quorum_net:
+    driver: bridge
+    ipam:
+      driver: default
+      config:
+      - subnet: $subnet
+EOF
+
+
+#### Create pre-populated contracts ####################################
+
+# Private contract - insert Node 2 as the recipient
+cat templates/contract_pri.js \
+    | sed s:_NODEKEY_:`cat qdata_2/keys/tm.pub`:g \
+          > contract_pri.js
+
+# Public contract - no change required
+cp templates/contract_pub.js ./
+echo 'Done!'
