9b9a9144d1279431b1924b6677e6f4a57c785bc4
Bruce Lee <coolcode@live.com>
2019-09-05 22:02:47 +0800

    :monkey_face: add tm.conf

--- Nnodes/deploy_tm.conf.sh
+++ Nnodes/deploy_tm.conf.sh
@@ -46,7 +46,8 @@ echo '[4] Creating Quorum keys and finishing configuration.'
 qd=qdata_0
 
 echo $((current_node))
-echo 'myip: '${ips[$current_node]}
+echo 'myip: '${ips[$((current_node))]}
+echo 'myip2: '${ips[$current_node]}
 cat templates/tm.conf \
     | sed s/_NODEIP_/${ips[$current_node]}/g \
     | sed s%_NODELIST_%$nodelist%g \
